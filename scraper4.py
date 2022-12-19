dacbb48250a36520dd663c3c68c756253fd2ef41
Simeon <simeon@pop-os.localdomain>
2022-12-19 22:53:04 +0200

    data gathered from dalivali scraper4

--- scraper4.py
+++ scraper4.py
@@ -23,7 +23,8 @@ cloud=[]
 sunrise=[]
 sundown=[]
 daylen=[]
-
+verbal=[]
+humidity=[]
 
 driver = webdriver.Chrome("./chromedriver", options=options,)
 driver.get(
@@ -34,8 +35,16 @@ d1 = today.strftime("%d/%m/%Y") # dd/mm/YY
 print(d1)
 days=driver.find_element(By.CLASS_NAME,'slick-track')
 day=days.find_elements(By.CLASS_NAME,'slick-slide')
-print(driver.find_element(By.ID,'title-day1').get_attribute("innerText"))
-# print(day[0].find_element(By.TAG_NAME,'li').find_element(By.TAG_NAME,'div').find_element(By.ID,'title-day1').text)
-# for i in range(0,9):
-    # dayoftheweek.append(day[i].find_element(By.ID,'title-day1').text)
-    # print(dayoftheweek[i])
+
+print(len(day))
+for i in range(0,9):
+    dayoftheweek.append(day[i].find_element(By.ID,'title-day1').get_attribute('innerText'))
+    exdate.append(day[i].find_element(By.ID,'date1').get_attribute('innerText'))
+    tmax.append(day[i].find_element(By.ID,'tNday1').get_attribute('innerText'))
+    tmin.append(day[i].find_element(By.ID,'tDday1').get_attribute('innerText'))
+    windspd.append(day[i].find_element(By.ID,'info-num-Spwind1').get_attribute('innerText'))
+    winddir.append(day[i].find_element(By.ID,'info-num-wind1').get_attribute('innerText'))
+    humidity.append(day[i].find_element(By.ID,'info-num-rain1').get_attribute('innerText'))
+    verbal.append(day[i].find_element(By.ID,'descr-day1').get_attribute('innerText'))
+    
+    print(dayoftheweek[i],exdate[i],tmax[i],tmin[i],windspd[i],winddir[i],humidity[i],verbal[i])

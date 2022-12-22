1dc8a68a23f6b88a5a4128ca6a2f75ec95bf0429
Simeon <simeon@pop-os.localdomain>
2022-12-22 12:50:31 +0200

    freemeteo - има всичката информация. Да се форматират данните sinopti- няма влажност. да се форматират данните. dalivali- има всичката информация. да се форматират данните за всички - да се направят изображенията.

--- scraper2.py
+++ scraper2.py
@@ -1,60 +1,44 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
-#from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
-from selenium import webdriver
 from selenium.webdriver.common.by import By
+from datetime import date, timedelta
 
-options = webdriver.ChromeOptions() 
+
+options = Options()
+options.add_argument('--headless')
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
 
-row=[]
-driver = webdriver.Chrome(".\chromedriver.exe", options=options,)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
 driver.get(
     "https://freemeteo.bg/weather/plovdiv/7-days/list/?gid=728193&language=bulgarian&country=bulgaria"
 )
-
-# day=driver.find_elements(By.CSS_SELECTOR,'#content > div.right-col > div.weather-now > div.today.table > div > div > div:nth-child(1)')
+row=[]
 day=driver.find_elements(By.CLASS_NAME,'day')
-# maxt=driver.find_elements(By.CLASS_NAME,'DetailsSummary--highTempValue--3Oteu')
-# mint=driver.find_elements(By.CLASS_NAME, 'DetailsSummary--lowTempValue--3H-7I')
-# percip=driver.find_elements(By.CLASS_NAME,'DetailsSummary--precip--1ecIJ')
-# verbal=driver.find_elements(By.CLASS_NAME,'DetailsSummary--extendedData--365A_')
 title=[]
 image=[]
 tmin=[]
 tmax=[]
 text=[]
-print(day[0].find_element(By.CLASS_NAME,'temps').find_elements(By.CSS_SELECTOR,'#content > div.right-col > div.weather-now > div.today.table > div > div > div:nth-child(2) > div.temps > b'))
+#print(day[0].find_element(By.CLASS_NAME,'temps').find_elements(By.CSS_SELECTOR,'#content > div.right-col > div.weather-now > div.today.table > div > div > div:nth-child(2) > div.temps > b'))
 # text=verbal[::2]
 # wind=verbal[1::2]
-for i in range(0,6):
-    title.append(day[i].find_element(By.CLASS_NAME,'title').find_element(By.TAG_NAME,'span')) #reaching furhter into the dom (gotta transform text to datetime)
-    tmax.append(day[i].find_element(By.CLASS_NAME,'temps').find_elements(By.CSS_SELECTOR,'#content > div.right-col > div.weather-now > div.today.table > div > div > div:nth-child(2) > div.temps > b'))
-    tmin.append(day[i].find_element(By.CLASS_NAME,'temps').find_element(By.TAG_NAME,'span'))
-    text.append(day[i].find_element(By.CLASS_NAME,'hover').find_element(By.CLASS_NAME,'info').find_element(By.CLASS_NAME,'extra'))
-    image.append(day[i].find_element(By.CLASS_NAME,'icon').find_element(By.TAG_NAME,'span'))
-    image[i].screenshot('./icon'+str(title[i].text)+'.png')
-    if len(tmax[i])==0:
-        tmax[i].text="N/A"
-    print(title[i].text+', ')
-    print(tmax[i].text+', ')
-    print(tmin[i].text+', ')
-    print(text[i].text)
-print(title)
 
-# for x in range(0,14):
-#     print("max temp is " + maxt[x].text
-#      + ', min temp is '+mint[x].text
-#      +' the weather is '+ text[x].get_attribute("innerText") 
-#      +' the chance of rain is '+percip[x].text
-#      +' the wind direction and speed is '+wind[x].get_attribute("innerText").lstrip("Wind"))
-
-
-
-
+print(len(day))
+for i in range(0,7):
+    title.append(day[i].find_element(By.CLASS_NAME,'title').find_element(By.TAG_NAME,'span').get_attribute('innerText')) #reaching furhter into the dom (gotta transform text to datetime)
+    #tmax.append(day[i].find_element(By.CLASS_NAME,'temps').find_elements(By.CSS_SELECTOR,'#content > div.right-col > div.weather-now > div.today.table > div > div > div:nth-child(2) > div.temps > b'))
+    tmax.append(day[i].find_element(By.CLASS_NAME,'temps').find_element(By.TAG_NAME,'b').get_attribute('innerText'))
+    tmin.append(day[i].find_element(By.CLASS_NAME,'temps').find_element(By.TAG_NAME,'span').get_attribute('innerText'))
+    text.append(day[i].find_element(By.CLASS_NAME,'hover').find_element(By.CLASS_NAME,'info').find_element(By.CLASS_NAME,'extra').get_attribute('innerText'))
+    # image.append(day[i].find_element(By.CLASS_NAME,'icon').find_element(By.TAG_NAME,'span'))
+    # image[i].screenshot('./icon'+str(title[i].text)+'.png')
+    # if len(tmax[i])==0:
+    #     tmax[i].text="N/A"
+ 
+    print(title[i],tmax[i],tmin[i],text[i])
 
 
 # image = driver.find_element(By.CLASS_NAME, "DailyContent--Condition--bQKA2")
--- scraper4.py
+++ scraper4.py
@@ -1,42 +1,33 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
-#from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from datetime import date, timedelta
-options = webdriver.ChromeOptions() 
+
+
+options = Options()
+options.add_argument('--headless')
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
-options.add_argument("--headless")
-row=[]
-dayoftheweek=[]
-exdate=[]
-tmax=[]
-tmin=[]
-windspd=[]
-winddir=[]
-rainchance=[]
-rainvolume=[]
-cloud=[]
-sunrise=[]
-sundown=[]
-daylen=[]
-verbal=[]
-humidity=[]
 
-driver = webdriver.Chrome("./chromedriver", options=options,)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
 driver.get(
-    "https://www.dalivali.bg/?type=daily&location=173"
+     "https://www.dalivali.bg/?type=daily&location=173"
 )
-today = date.today()
-d1 = today.strftime("%d/%m/%Y") # dd/mm/YY
-print(d1)
-days=driver.find_element(By.CLASS_NAME,'slick-track')
-day=days.find_elements(By.CLASS_NAME,'slick-slide')
 
-print(len(day))
+row,dayoftheweek,exdate,tmax,tmin,windspd,winddir,rainchance,rainvolume,cloud,sunrise,sundown,daylen,verbal,humidity = ( [] for i in range(15))
+
+today = date.today().strftime("%d/%m/%Y") # dd/mm/YY
+print('Todays date is ' + today)
+
+days = driver.find_element(By.CLASS_NAME,'slick-track')
+day = days.find_elements(By.CLASS_NAME,'slick-slide')
+
+print(str(len(day))+' days are visible from www.dalivali.bg in the city of Plovdiv')
+print('DayOfTheWeek'.ljust(10), ' ','Date'.ljust(10), ' ', 'MinTemp'.ljust(10), ' ', 'MaxTemp'.ljust(10), ' ',
+ 'WindSpeed'.ljust(10), ' ', 'WindDir'.ljust(10), ' ', 'Humidity'.ljust(10), ' ', 'Decription'.ljust(10), ' ' )
+
 for i in range(0,9):
     dayoftheweek.append(day[i].find_element(By.ID,'title-day1').get_attribute('innerText'))
     exdate.append(day[i].find_element(By.ID,'date1').get_attribute('innerText'))
@@ -47,4 +38,6 @@ for i in range(0,9):
     humidity.append(day[i].find_element(By.ID,'info-num-rain1').get_attribute('innerText'))
     verbal.append(day[i].find_element(By.ID,'descr-day1').get_attribute('innerText'))
     
-    print(dayoftheweek[i],exdate[i],tmax[i],tmin[i],windspd[i],winddir[i],humidity[i],verbal[i])
+for i in range(0,9):    
+    print(dayoftheweek[i].ljust(11), ' ',exdate[i].ljust(11), ' ', tmax[i].ljust(11), ' ', tmin[i].ljust(11), ' ',
+     windspd[i].ljust(11), ' ', winddir[i].ljust(11), ' ', humidity[i].ljust(11), ' ', verbal[i].ljust(11), ' ' )
